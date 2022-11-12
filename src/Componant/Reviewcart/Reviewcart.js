02267cc15faf9c196628a7ef389eb8ae67c9dc02
nazmul <nazmulalam9203@gmail.com>
2022-11-13 02:29:01 +0600

    notfound page

--- src/Componant/Reviewcart/Reviewcart.js
+++ src/Componant/Reviewcart/Reviewcart.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const Reviewcart = (props) => {
-    const {name,username} = props.product;
+    const {name,username,id} = props.product;
     console.log(props)
     return (
         <div className='mt-4 border border-solid border-2 border-indigo-600 py-5 ml-6 pl-4 mr-5'>
              <h1><span className='mr-2'>Name:</span>{username} </h1>
              <h1><span className='mr-2'>Review:</span>{name} </h1>
+             <h1> Raiting: {id} out of 15</h1>
            
         </div>
     );
