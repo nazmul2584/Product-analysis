47b364fe827bfe8671913b42241b076ab15d8a3d
nazmul <nazmulalam9203@gmail.com>
2022-11-15 10:49:13 +0600

    chart added

--- src/Componant/Blog/Blog.js
+++ src/Componant/Blog/Blog.js
@@ -3,7 +3,17 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h1>this is blog</h1>
+            
+            <h1 className='mb-4 text-lg'>1.context api⁇   </h1>
+            <h1>Ans:  Normally when we pass data varius component using props, but when we have 
+                more then 10 component or above it will more easy to pass dada using 
+                context api for certain component. </h1>
+                <h1 className='mb-4 mt-2 text-lg'>2.what is semantic tag⁇   </h1>
+                <h1>
+                Ans:  Semantic HTML is the use of HTML markup to reinforce the semantics,
+                 or meaning, of the information in web pages and web applications rather than
+                  merely to define its presentation or look
+                </h1>
         </div>
     );
 };
--- src/Componant/Home/Home.js
+++ src/Componant/Home/Home.js
@@ -16,9 +16,13 @@ const Home = () => {
                 <div className='flex border border'>
                     <div className='txt-container flex-1 w-1/2 text-5xl mt-9'>
                         
-                        <h1 className='mb-5  text-orange-600 font-semibold'>YOER NEXT LAPTOP</h1>
-                        <h1 className='mb-5'>YOER BEST  LAPTOP</h1>
-                        <p className='text-2xl text-left ml-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, unde eveniet quidem cupiditate officia numquam obcaecati eos, placeat consequatur quisquam incidunt assumenda iusto magni. Hic commodi possimus corrupti nobis officiis?</p>
+                        <h1 className='mb-5  text-orange-600 font-semibold'>YOUR NEXT LAPTOP</h1>
+                        <h1 className='mb-5'>YOUR BEST  LAPTOP</h1>
+                        <p className='text-2xl text-left ml-4 text-justify mr-4 mb-3'>For all types of working
+                         styles, Dell has introduced their monitor which can make your work life 
+                         much easier. This monitor comes with huge screen size ranges is more than ca
+                         pable of providing you with HD resolution to Ultra HD resolution. With slim and ergonomically pleasing-looking, you will also be able to position these monitors in the way of your choice and can enjoy your way of working. Smart Technologies have bought a huge collecti
+                        on of these Dell monitors for their all type of users within a reasonable price range</p>
                     </div>
                     <div className='text-container flex-1 w-1/2 mr-10 mt-9'>
                         
